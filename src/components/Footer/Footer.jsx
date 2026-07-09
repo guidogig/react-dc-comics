b193b7567924b3c625091485bb09cf94529f1d65
Guido Giglioli <guido.tobugroup@gmail.com>
2026-07-10 00:00:58 +0200

    iniziato a lavorare su footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,52 @@
 import './Footer.css'
-import FooterFeatures from './FooterFeatures'
-import FooterNav from './FooterNav'
-import FooterCTA from './FooterCTA'
+import footerFeatures from '../../data/footerFeatures'
+
+function FooterFeatures() {
+    return (
+        <div className="footer_features">
+            <div className="container">
+                <ul>
+                    {footerFeatures.map(feature =>{
+                        return (
+                            <li>
+                                <img key={feature.key} src={feature.src} alt={feature.title} />
+                                <span>{feature.title}</span>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function FooterNav() {
+    return (
+        <div className="footer_nav">
+            <div className="container">
+                Footer Nav
+            </div>
+        </div>
+    )
+}
+
+function FooterCTA() {
+    return (
+        <div className="footer_CTA">
+            <div className="container">
+                Footer CTA
+            </div>
+        </div>
+    )
+}
 
 
 function Footer() {
     return (
         <footer className='footer'>
-            <FooterFeatures/>
-            <FooterNav/>
-            <FooterCTA/>
+                <FooterFeatures />
+                <FooterNav />
+                <FooterCTA />
         </footer>
     )
 }
---
+++ src/data/footerFeatures.js
@@ -0,0 +1,29 @@
+const footerFeatures = [
+    {
+        key: 1,
+        title: "DIGITAL COMICS",
+        src: "/img/buy-comics-digital-comics.png",
+    },
+    {
+        key: 2,
+        title: "DC MERCHANDISE",
+        src: "/img/buy-comics-merchandise.png",
+    },
+    {
+        key: 3,
+        title: "SUBSCRIPTION",
+        src: "/img/buy-comics-subscriptions.png",
+    },
+    {
+        key: 4,
+        title: "COMIC SHOP LOCATOR",
+        src: "/img/buy-comics-shop-locator.png",
+    },
+    {
+        key: 5,
+        title: "DC POWER VISA",
+        src: "/img/buy-dc-power-visa.svg",
+    },
+];
+
+export default footerFeatures;
