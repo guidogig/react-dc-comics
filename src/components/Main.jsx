922ca6cf917da8ad76fe42a9d6ad3b2b7dc80926
Guido Giglioli <guido.tobugroup@gmail.com>
2026-07-11 17:37:35 +0200

    completato Main()

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,31 @@ import "./Main.css";
 function Main({ comics }) {
   return (
     <main>
-      <p>--&gt; Content Goes Here &lt;--</p>
+      <div className="jumbotron"></div>
+      <div className="badge-series">
+        <section>CURRENT SERIES</section>
+      </div>
+      <Comics comics={comics} />
+      <div className="btn-container">
+        <button className="load-more-btn">LOAD MORE</button>
+      </div>
     </main>
   );
 }
 
+function Comics({ comics }) {
+  return (
+    <div className="comicsSection">
+      {comics.map((comic) => (
+        <div key={comic.id} className="comic">
+          <img src={comic.thumb} alt={comic.title} className="cover" />
+          <p className="title">{comic.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default Main;
+
+/* <img className="jumbotron" src="./img/jumbotron.jpg" alt="jumbotron" /> */
